04a60192cf5427700a4d3a93240bf8300e73ce3d
Lucyna Rudzka <lrudzka@onet.eu>
2018-07-18 09:57:20 +0200

    20180718-02

--- js/AboutMe.jsx
+++ js/AboutMe.jsx
@@ -9,18 +9,29 @@ class AboutMe extends React.Component{
         }
     }
 
-    handleclickLvl1 = () => {
-        this.setState({
-            lvl1: 1
-        })
+    handleClickLvl1 = () => {
+        if (this.state.lvl1 === 0) {
+            this.setState({
+                lvl1: 1
+            })
+        } else {
+            this.setState({
+                lvl1: 0
+            })
+        }
     }
 
     render(){
+
+        let style;
+        if (this.state.lvl1 === 1) {
+            style = {marginTop: 0}
+        }
         return(
             <section id="body">
-                <div className="line"><div onClick = {this.handleclickLvl1} className="pointer"></div></div>
-                {this.state.lvl1 > 0 && <Introduction/> }
-                <div className="line"><div className="pointer"></div></div>
+                <div className="line"><div onClick = {this.handleClickLvl1} className="pointer">FOR THE BEGINNING</div></div>
+                {this.state.lvl1 ===1 && <Introduction/> }
+                <div style = {style}  className="line"><div className="pointer">MY PORTFOLIO</div></div>
             </section>
         )
     }
