2400ccfefca118b033f1c670cd2a94364ac455e2
Lucyna Rudzka <lrudzka@onet.eu>
2018-07-22 22:50:25 +0200

    20180722-06

--- js/Footer.jsx
+++ js/Footer.jsx
@@ -57,10 +57,10 @@ class Footer extends React.Component{
                     <div>
                         <span><span className="color"> CONTACT:</span> lrudzka@onet.eu</span>
                     <a href="https://github.com/lrudzka" target="_blank">
-                        <img src="../img/github.png" alt="my github profile" title="my github profile" style={style1} />
+                        <img src="images/github.png" alt="my github profile" title="my github profile" style={style1} />
                     </a>
                     <a href="https://www.linkedin.com/in/lucyna-rudzka/" target="_blank">
-                        <img src="../img/linkedin.png" alt="my linkedin profile" title="my linkedin profile" style={style2} />
+                        <img src="images/linkedin.png" alt="my linkedin profile" title="my linkedin profile" style={style2} />
                     </a>
                     </div>
                 </section>
